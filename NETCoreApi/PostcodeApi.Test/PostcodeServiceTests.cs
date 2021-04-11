a36bb66293cfa78f35fd2742c51f0a6a48fa4944
Sam Burdon <Sam.Burdon@oakbrookfinance.com>
2021-04-11 21:50:59 +0100

    Add first passing unit test

--- NETCoreApi/PostcodeApi.Test/PostcodeServiceTests.cs
+++ NETCoreApi/PostcodeApi.Test/PostcodeServiceTests.cs
@@ -1,14 +1,46 @@
+using System.Net;
+using System.Net.Http;
 using FluentAssertions;
+using FluentAssertions.Execution;
+using Moq;
+using Newtonsoft.Json;
 using NUnit.Framework;
+using PostcodeApi.Application;
+using PostcodeApi.Domain;
 
 namespace PostcodeApi.Test
 {
     public class Tests
     {
         [Test]
-        public void TrueShouldBeTrue()
+        public void WhenResponseIsSuccess_ReturnPostcodeResponse()
         {
-            true.Should().BeTrue();
+            var expectedResponse = new PostcodeResponse
+            {
+                Result = new LocationData
+                {
+                    Latitude = 1,
+                    Longitude = 1
+                }
+            };
+
+            var json = JsonConvert.SerializeObject(expectedResponse);
+
+            var httpResponse = new HttpResponseMessage();
+            httpResponse.StatusCode = HttpStatusCode.OK;
+            httpResponse.Content = new StringContent(json);
+
+            var mockMapper = new Mock<IPostcodeIoApiWrapper>();
+            mockMapper.Setup(x => x.Get(It.IsAny<string>())).ReturnsAsync(httpResponse);
+
+            var sut = new PostcodeService(mockMapper.Object);
+
+            var result = sut.GetPostcodeLocation("NG15AD");
+
+            using (new AssertionScope())
+            {
+                result.Result.Should().BeEquivalentTo(expectedResponse);
+            }
         }
     }
 }
